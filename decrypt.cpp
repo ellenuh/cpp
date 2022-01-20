cd185ddef443e7c09df68d53c21c20bddfe13f70
elena <91695982+ellenuh@users.noreply.github.com>
2022-01-21 00:16:44 +0200

    Create decrypt.cpp

---
+++ decrypt.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int i,j,k;
+    string s,t;
+    int key;
+    cin>>key;
+    cin>>s;
+    for(i=0;i<s.size();i++){
+        t+=(s[i]-key+26)%26;  //?
+    }
+    cout<<t<<'\n';
+    return 0;
+}
+
+//ceasar cipher decryptor.
